9cd20fb12cefe5368eac814aaf28cb4ef195e1ae
MotivDesktop1 <calebw@motiv.io>
2022-06-07 16:30:13 +1200

    gap test

--- ngx_chart_test/src/app/line-chart/data.service.ts
+++ ngx_chart_test/src/app/line-chart/data.service.ts
@@ -241,50 +241,76 @@ data4 = [
       },
     ]
   },
-
   {
-    "name": "USA",
+    "name": "Germany",
     "series": [
       {
-        "name": new Date("2010-01-01"),
-        "value": 7870000
+        "name": new Date("2005-01-01"),
+        "value": 7300000
       },
       {
-        "name": new Date("2011-01-01"),
-        "value": 8270000
+        "name": new Date("2006-01-01"),
+        "value": 8940000
       },
       {
-        "name": new Date("2012-01-01"),
-        "value": 10940000
-      },
-      {
-        "name": new Date("2013-01-01"),
+        "name": new Date("2007-01-01"),
         "value": 9940000
       },
+      {
+        "name": new Date("2008-01-01"),
+        "value": 8940000
+      }
+      // ,
+      // {
+      //   "name": new Date("2009-01-01"),
+      //   "value": 10940000
+      // }
     ]
   },
 
-  {
-    "name": "France",
-    "series": [
-      {
-        "name": new Date("2010-01-01"),
-        "value": 5000002
-      },
-      {
-        "name": new Date("2011-01-01"),
-        "value": 5800000
-      },
-      {
-        "name": new Date("2012-01-01"),
-        "value": 6940000
-      },
-      {
-        "name": new Date("2013-01-01"),
-        "value": 7940000
-      },
-    ]
-  }
+  // {
+  //   "name": "USA",
+  //   "series": [
+  //     {
+  //       "name": new Date("2010-01-01"),
+  //       "value": 7870000
+  //     },
+  //     {
+  //       "name": new Date("2011-01-01"),
+  //       "value": 8270000
+  //     },
+  //     {
+  //       "name": new Date("2012-01-01"),
+  //       "value": 10940000
+  //     },
+  //     {
+  //       "name": new Date("2013-01-01"),
+  //       "value": 9940000
+  //     },
+  //   ]
+  // },
+
+  // {
+  //   "name": "France",
+  //   "series": [
+  //     {
+  //       "name": new Date("2010-01-01"),
+  //       "value": 5000002
+  //     },
+  //     {
+  //       "name": new Date("2011-01-01"),
+  //       "value": 5800000
+  //     },
+  //     {
+  //       "name": new Date("2012-01-01"),
+  //       "value": 6940000
+  //     },
+  //     {
+  //       "name": new Date("2013-01-01"),
+  //       "value": 7940000
+  //     },
+  //   ]
+  // }
 ];
 
 data5=[
